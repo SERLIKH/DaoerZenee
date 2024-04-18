f3536b1cc13bcd2e21ff857ed385c5ff1567cec5
SERLIKH <91310029+SERLIKH@users.noreply.github.com>
2024-04-18 09:57:45 +0700

    Add files via upload

---
+++ beranda.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="background.css">
+    <title>Daoer Zenee | Beranda</title>
+  </head>
+  <body>
+    <div class="page-content">
+      <nav class="navbar navbar-expand-lg navbar-light bg-custom" id="my-navbar">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">
+            <img src="logoNama.png" alt="Daoer Zenee" width="230" height="82.62">
+          </a>
+          <span>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Beranda</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/produk">Produk</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/profil">Profil Komunitas</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/galeri">Galeri</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/hubungikami" >Hubungi Kami</a>
+              </li>
+            </ul>
+          </div>
+          </span>
+        </div>
+      </nav>
+    </div>
+    <div class="container-fluid position-relative">
+      <div class="bg-image">
+        <img src="BannerHome.png" alt="Banner Home" width="100%" height="auto" class="banner-image">
+      </div>
+      <div class="intro-content">
+        <h1 class = "welcomeTxt">Welcome to Daoer Zenee!</h1>
+        <p class="subWelcomeTxt">Explore our products, gallery, and much more!</p>
+      </div>
+      <!-- ...rest of the code... -->
+    </div>
+  </body>
+</html>
